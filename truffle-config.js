a3f53a8b68c2af03207be86b52ba5ef000e77f61
hwangby912 <quddbs912@naver.com>
2019-09-10 15:27:55 +0900

    Token

---
+++ truffle-config.js
@@ -0,0 +1,19 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+ 
+var infura_apikey = process.env.API_KEY;
+var mnemonic = process.env.MNEMONIC;
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+      network_id: 3
+    }
+  }
+};
